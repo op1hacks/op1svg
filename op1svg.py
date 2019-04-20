f80ba49dd45a841515b16fe87380f9141bc7c7aa
Richard Lewis <richrd.lewis@gmail.com>
2019-04-20 18:46:28 +0300

    Restructure code and add setup file.

---
+++ op1svg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Convenience wrapper for running op1svg directly from source tree."""
+
+from op1svg.main import main
+
+if __name__ == "__main__":
+    main()
